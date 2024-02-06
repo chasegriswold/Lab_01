208b5bce3dd224f790de097f188c917b5a95ff6a
chasegriswold <chase.griswold@gmail.com>
2024-02-05 22:22:27 -0700

    After Correcting Error Backup
    Fixed, should run

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -62,41 +62,147 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+//	---------- Uncomment for First checkoff ----------
+//	
+///*blue and red LEDs are attached to pins 6 & 7 respectively*/	
+//	
+//// (1) Enable the peripheral clock of GPIOC 
+//	RCC->AHBENR |= RCC_AHBENR_GPIOCEN; /* (1) */
+//	// Input-Output
+//	/* Set Red LED On*/
+//	GPIOC->MODER |= (1<<12); // Sets the 12th bit = 1
+//	GPIOC->MODER &= ~(1<<13); // Set 13th bit = 0
+//	/* Set Blue LED On*/
+//	GPIOC->MODER |= (1<<14); // Sets the 14th bit = 1
+//	GPIOC->MODER &= ~(1<<15); // Set 15th bit = 0
+//	
+//	// Push-Pull
+//	/* Set Red LED push-pull*/
+//	GPIOC->OTYPER &= ~(1<<6); // Sets the 6th bit = 0 -- NOTE: 0 sets push pull in OTYPER
+//	/* Set Blue LED On*/
+//	GPIOC->OTYPER &= ~(1<<7); // Sets the 7th bit = 0 -- NOTE: 0 sets push pull in OTYPER
+//	
+//	// Set Speed
+//	//x0: Low speed (first bit is a don't care)
+//		/* Set Red LED Speed */
+//	GPIOC->OSPEEDR &= ~(1<<12); // Sets the 12th bit = 0 -- NOTE: Next bit does not matter
+//	/* Set Blue LED Speed */
+//	GPIOC->OSPEEDR &= ~(1<<14); // Sets the 14th bit = 0 -- NOTE: Next bit does not matter
+//	
+//		/* Set Red LED no-pullupdown */
+//	GPIOC->PUPDR &= ~(1<<12); // Sets the 12th bit = 0
+//	GPIOC->PUPDR &= ~(1<<13); // Sets the 13th bit = 0
+//	/* Set Blue LED no-pullupdown */
+//	GPIOC->PUPDR &= ~(1<<14); // Sets the 14th bit = 0
+//	GPIOC->PUPDR &= ~(1<<15); // Sets the 15th bit = 0
+//	
+//	SystemClock_Config(); //Configure the system clock
+//	/* The above is for the HAL_delay function */
 
-  /* MCU Configuration--------------------------------------------------------*/
+//	while (1) {
+//		
+//		HAL_Delay(400); // Delay 400ms
+//		
+//			/*set red high and blue low*/
+//			/* Set Red LED High */
+//			GPIOC->ODR |= (1<<6); // Sets the 6th bit = 1 
+//			/* Set Blue LED Low */
+//			GPIOC->ODR &= ~(1<<7); // Sets the 7th bit = 0
+//		
+//		HAL_Delay(400); // Delay 400ms
+//		
+//		/*toggle*/
+//			/*set red low and blue high*/
+//			/* Set Red LED High */
+//			GPIOC->ODR &= ~(1<<6); // Sets the 6th bit = 0 
+//			/* Set Blue LED Low */
+//			GPIOC->ODR |= (1<<7); // Sets the 7th bit = 1		
+//		
+//	}
+//	
+// ------------ End of first checkoff -----------
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
 
-  /* USER CODE BEGIN Init */
+// ---------- Checkoff Part 2 -----------
 
-  /* USER CODE END Init */
+/*blue and red LEDs are attached to pins 6 & 7 respectively*/	
+	
+// (1) Enable the peripheral clock of GPIOC 
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN; /* (1) */
+	// Input-Output
+	/* Set Red LED On*/
+	GPIOC->MODER |= (1<<12); // Sets the 12th bit = 1
+	GPIOC->MODER &= ~(1<<13); // Set 13th bit = 0
+	/* Set Blue LED On*/
+	GPIOC->MODER |= (1<<14); // Sets the 14th bit = 1
+	GPIOC->MODER &= ~(1<<15); // Set 15th bit = 0
+	
+	// Push-Pull
+	/* Set Red LED push-pull*/
+	GPIOC->OTYPER &= ~(1<<6); // Sets the 6th bit = 0 -- NOTE: 0 sets push pull in OTYPER
+	/* Set Blue LED On*/
+	GPIOC->OTYPER &= ~(1<<7); // Sets the 7th bit = 0 -- NOTE: 0 sets push pull in OTYPER
+	
+	// Set Speed
+	//x0: Low speed (first bit is a don't care)
+		/* Set Red LED Speed */
+	GPIOC->OSPEEDR &= ~(1<<12); // Sets the 12th bit = 0 -- NOTE: Next bit does not matter
+	/* Set Blue LED Speed */
+	GPIOC->OSPEEDR &= ~(1<<14); // Sets the 14th bit = 0 -- NOTE: Next bit does not matter
+	
+		/* Set Red LED no-pullupdown */
+	GPIOC->PUPDR &= ~(1<<12); // Sets the 12th bit = 0
+	GPIOC->PUPDR &= ~(1<<13); // Sets the 13th bit = 0
+	/* Set Blue LED no-pullupdown */
+	GPIOC->PUPDR &= ~(1<<14); // Sets the 14th bit = 0
+	GPIOC->PUPDR &= ~(1<<15); // Sets the 15th bit = 0
+	
+	SystemClock_Config(); //Configure the system clock
+	/* The above is for the HAL_delay function */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+//This is technically the start of Checkoff part 2, but we needed to initialize the LEDs for this code as well.
+// Pushbuttons B1 USER: User and Wake-Up button connected to the I/O PA0 of the
+//this is on the GPIOA bus since it's PA0
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; //  Analog GPIO configuration
+	
+	/* Set PA0 to input mode (bits 0 and 1 of GPIOA) On*/
+	GPIOA->MODER = 0x28000000; // This is the reset state for GPIOA datasheet page 158
+	// the above code is a cleaner way to set it to input mode. The datasheet calls out bits 0 and 1 both be set to 0 for input mode, but above code accomplishes the same thing.
+	
+	//Set the pins to low speed
+	GPIOA->OSPEEDR &= ~(1<<0); // Sets the 0th bit = 0 -- NOTE: Next bit is a don't care
+	
+	//Enable the pull-down resistor --
+	GPIOA->PUPDR |= 2; // Since pull down is 10 for bits 1 and 0 respectively (2 in decimal) we can just set this register to = 2.
+	
+	GPIOC->ODR |= (1<<6); // 6th bit = 1
+	GPIOC->ODR &= ~(1<<7); // 7th bit = 0
+	
+	uint32_t debouncer = 0;
+	while(1) {
+		debouncer = (debouncer << 1); // Always shift every loop iteration
+		if (GPIOA->IDR & 0x01) { // If input signal is set/high //NOTE: the number 0x01 is literally bit 0 and this checks if it reads a high.
+		debouncer |= 0x01; // Set lowest bit of bit-vector
+		}
+		if (debouncer == 0xFFFFFFFF) {
+		// This code triggers repeatedly when button is steady high!
+		}
+		if (debouncer == 0x00000000) {
+		// This code triggers repeatedly when button is steady low!
+		}
+		if (debouncer == 0x7FFFFFFF) {
+		// This code triggers only once when transitioning to steady high!
+				GPIOC->ODR ^= (1<<6); // 6th bit = 0
+				GPIOC->ODR ^= (1<<7); // 7th bit = 0
+		}
+		// When button is bouncing the bit-vector value is random since bits are set when
+		//the button is high and not when it bounces low.
+		HAL_Delay(2); // Delay 2ms to avoid errors, see lab manual
+	}
 }
+
 
 /**
   * @brief System Clock Configuration
